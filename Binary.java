a3fcccc03723c5cb7fbf0333c0e8662d0b2e96bd
sasichintada <sasichinthada248@gmail.com>
2024-08-05 11:54:11 +0530

    Add files via upload

---
+++ Binary.java
@@ -0,0 +1,20 @@
+class Binary {
+	public static void main(String args[]) {
+	 int A[]={3,13,23,31,45,67,80};
+	 int key=67;
+	 System.out.print(key+"is found at index: "+ Binary(A,key));
+	}
+	public static int Binary(int A[],int key) {
+		int low=0,high=A.length,mid;
+		while(low<=high) {
+			mid=(low+high)/2;
+			if(A[mid]>=key)
+				return mid;
+			if(key>A[mid])
+				low=mid+1;
+			else
+				high=mid-1;
+	 	}
+	 	return -1;
+	 }
+}
